704df40d3eb431d82208a51a3632fc0b7f9a3db3
ZahraAlafoo <201700531@student.polytechnic.bh>
2022-05-24 09:35:26 +0300

    edit design

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,7 +15,7 @@ Auth.configure(awsConfig);
           <!-- Sales Card -->
 
           <!-- Reports -->
-          <div class="col-12">
+          <div class="col-11">
             <div
               style="padding: var(--amplify-space-large); text-align: center"
             >
--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -27,12 +27,7 @@ Auth.configure(awsConfig);
 
         <nav class="header-nav ms-auto">
           <ul class="d-flex align-items-center">
-            <li class="nav-item d-block d-lg-none">
-              <a class="nav-link nav-icon search-bar-toggle" href="#">
-                <i class="bi bi-search"></i>
-              </a>
-            </li>
-            <!-- End Search Icon-->
+           
 
             <li class="nav-item dropdown pe-3">
               <!-- End Profile Iamge Icon -->
