62bf2b15ad423aa6e806e3a6537616349b57f549
ZahraAlafoo <201700531@student.polytechnic.bh>
2022-05-24 10:40:34 +0300

    edit design8

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -15,7 +15,7 @@ Auth.configure(awsConfig);
           <!-- Sales Card -->
 
           <!-- Reports -->
-          <div class="col-11" style="margin-left: 2%;">
+          <div class="col-11" style="margin-left: 5%;">
             <div
               style="padding: var(--amplify-space-large); text-align: center"
             >
